223ce1ee3c31a2c34515227f045ca20155053dc6
ssmith <shawn.t.smith.777@gmail.com>
2020-09-06 14:19:51 -0500

    Add Linter Config

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const express = require("express");
-const logger = require("morgan");
-const { response } = require("express");
+const express = require('express');
+
+const logger = require('morgan');
+
 const app = express();
 
-//middlewares
-app.use(logger("dev"));
-//routes
-app.get("/", (req, res, next) => {
+// middle wares
+app.use(logger('dev'));
+// routes
+app.get('/', (req, res, next) => {
   res.status(200).json({
-    message: "you requested index page ..hi",
+    message: 'you requested index page ..hi',
   });
 });
-//catch 404 and forward to error func
+// catch 404 and forward to error func
 app.use((req, res, next) => {
-  const err = new Error("Not found");
+  const err = new Error('Not found');
   err.status = 404;
   next(err);
 });
 
-//Error Handeler func
+// Error Handler func
 app.use((err, req, res, next) => {
-  const error = app.get("env") === "development" ? err : {};
+  const error = app.get('env') === 'development' ? err : {};
   const status = err.status || 500;
 
-  //respond to client
+  // respond to client
   res.status(status).json({
     error: {
       message: error.message,
     },
   });
 
-  // respond to ourselfves
+  // respond to ourselves
   console.log(err);
 });
 
 // start server
-
-const port = app.get("port") || 5000;
+const port = app.get('port') || 5000;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
